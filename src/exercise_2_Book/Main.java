fd2b9b2ab8eae68f060ef9c7ce91b5c4e59f6493
Andrii.Kanevski <MAX@345ee>
2022-10-28 22:34:59 +0200

    JavaEssential_1/lesson 1

--- src/exercise_2_Book/Main.java
+++ src/exercise_2_Book/Main.java
@@ -16,12 +16,12 @@ public class Main {
         System.out.println("Введіть зміст книги :");
         book1.bookContent.content=scanner.nextLine();
 
+        // стандартне виконання згідно завданню
         book1.bookAuthor.show();
         book1.bookTitle.show();
         book1.bookContent.show();
 
-        //Бонусний функціонал
-
+        //Бонусний функціонал- додатковий метод в классі Book який збирає все до купи
         Book.showAll(book1);
     }
 
