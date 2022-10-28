12e1f00dea11064924d4c558ae3dbe8a160a6de4
Andrii.Kanevski <MAX@345ee>
2022-10-28 22:22:30 +0200

    JavaEssential_1/lesson 1

--- src/exercise_2_Book/Main.java
+++ src/exercise_2_Book/Main.java
@@ -1,11 +1,20 @@
 package exercise_2_Book;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book();
-        book1.bookAuthor.author="Тарас Шевченко";
-        book1.bookTitle.title="Заповіт";
-        book1.bookContent.content="Як умру то поховайте, мене на могилі...";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть автора книги :");
+        book1.bookAuthor.author= scanner.nextLine();
+
+        System.out.println("Введіть назву книги :");
+        book1.bookTitle.title=scanner.nextLine();
+
+        System.out.println("Введіть зміст книги :");
+        book1.bookContent.content=scanner.nextLine();
 
         book1.bookAuthor.show();
         book1.bookTitle.show();
